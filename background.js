bddda034af5c5c5f9731da5a3f53cd2e2f86ac6c
Michael Harper <info@michaelharper.net>
2024-07-09 10:14:46 -0400

    Working version. Addressed the background.js Uncaught Promise error.

--- background.js
+++ background.js
@@ -1,10 +1,16 @@
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
     if (changeInfo.status === 'complete') {
-        chrome.storage.sync.get('pasteEnabled', function(data) {
-            chrome.tabs.sendMessage(tabId, {
-                action: 'updatePasteState',
-                isEnabled: data.pasteEnabled || false
-            });
+        chrome.storage.sync.get(['pasteEnabled', 'enabledUrl'], function(data) {
+            if (tab.url === data.enabledUrl) {
+                chrome.tabs.sendMessage(tabId, {
+                    action: 'updatePasteState',
+                    isEnabled: data.pasteEnabled || false
+                }, function(response) {
+                    if (chrome.runtime.lastError) {
+                        // Silently ignore the error
+                    }
+                });
+            }
         });
     }
 });
